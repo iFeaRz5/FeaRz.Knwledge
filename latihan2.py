dff91dfc4bc45285e9fc77ccb7d1c4a52c116f3e
iFeaRz5 <destayogapratama2005@gmail.com>
2024-10-24 21:07:56 +0700

    Input & Output
    Proses input dan output

---
+++ latihan2.py
@@ -0,0 +1,16 @@
+print("=+=+=+=+ Data Diri Mahsiswa =+=+=+=+")
+#proses input
+nim = input("NIM : ")
+nama = input("NAMA LENGKAP : ")
+jurusan =  input("JURUSAN : ")
+alamat = input("ALAMAT : ")
+nilai = input("NILAI")
+
+print("--------------------------------------- ")
+#proses output
+print("hasil cetak data diatas adalah")
+print("NIM :" +str(nim))
+print("NAMA :" +str(nama))
+print("jurusan :" +str(jurusan))
+print("ALAMAT :" +str(alamat))
+print("NILAI :" +(nilai)) 
